4e7b17ed4362c535c2b6fb919c3681934b5b0228
qbll9 <qmaccba@gmail.com>
2025-02-21 09:31:07 -0800

    feat: Initialize db connections to postgres server with sqlx

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::net::SocketAddr;
+#[macro_use]
+extern crate log;
 
+extern crate pretty_env_logger;
 use axum::{
     routing::{delete, get, post},
     Router,
@@ -8,10 +10,35 @@ use axum::{
 mod handlers;
 mod models;
 
+use dotenvy::dotenv;
 use handlers::*;
+use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() {
+    // Initialize pretty_env_logger
+    pretty_env_logger::init();
+
+    // Initialize dotenv
+    dotenv().ok();
+
+    // Create a new PgPoolOptions instance with a maximum of 5 connections.
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&std::env::var("DATABASE_URL").expect("DATABASE_URL must be sent."))
+        .await
+        .expect("Failed to create Postgres connection pool!");
+
+    // Using slqx, execute a SQL query that selects all questions from the questions table.
+    let recs = sqlx::query!("SELECT * FROM questions")
+        .fetch_all(&pool)
+        .await
+        .unwrap();
+
+    info!("********* Question Records *********");
+    // Log recs with debug formatting using the info! macro
+    info!("{:?}", recs);
+
     let app = Router::new()
         .route("/question", post(create_question))
         .route("/questions", get(read_questions))
